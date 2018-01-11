9097c676707f53759071937af21de88145f32499
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-11 22:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  let i = 0;
+  
   if (item.hasOwnProperty(item))
+    delete ite
 }
 
 function placeOrder(cardNumber) {
