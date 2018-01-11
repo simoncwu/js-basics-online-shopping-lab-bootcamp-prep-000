e4815841d7f5830a14650ad536f2a4e4cd0a173e
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-11 21:56:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,32 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push({ [item] : Math.floor(Math.random() * 99 + 1) });
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  let s = 'Your shopping cart is empty.';
+  if (cart.length > 0) {
+    s = 'In your cart, you have ';
+    let sep = cart.length > 2 ? ', ' : ' ';
+    for (let i = 0; i < cart.length; i++) {
+      if (cart.length > 1) {
+        if (i > 0) {
+          s += sep;
+          if (i == cart.length - 1) {
+            s += 'and ';
+          }
+        }
+      }
+      let v = Object.values(cart[i]);
+      s += `${v[0]} at \$${v[1]}`;
+    }
+    s += '.';
+  }
+  console.log(s);
 }
 
 function total() {
