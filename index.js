e91292b302761645f4462a54ead7a2f8b74438f9
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-11 22:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,5 +62,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber)
+    console.log("Sorry, we don't have a credit card on file for you.");
+  else {
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.);
+    
+  }
 }
